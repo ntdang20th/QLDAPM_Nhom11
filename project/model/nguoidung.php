1c1862c0d6dcdf387e4e808bdc16ebe30101c55a
Dang <ntdang_20th@student.agu.edu.vn>
2022-11-10 20:35:42 +0700

    them model nguoidung

---
+++ project/model/nguoidung.php
@@ -0,0 +1,24 @@
+<?php
+class NGUOIDUNG{
+    // authentication
+	public function kiemtranguoidunghople($email,$matkhau){
+		$db = DATABASE::connect();
+		try{
+			$sql = "SELECT * FROM nguoidung WHERE email=:email AND matkhau=:matkhau AND trangthai=1";
+			$cmd = $db->prepare($sql);
+			$cmd->bindValue(":email", $email);
+			$cmd->bindValue(":matkhau", md5($matkhau));
+			$cmd->execute();
+			$valid = ($cmd->rowCount () == 1);
+			$cmd->closeCursor ();
+			return $valid;			
+		}
+		catch(PDOException $e){
+			$error_message=$e->getMessage();
+			echo "<p>Lỗi truy vấn: $error_message</p>";
+			exit();
+		}
+	}
+}
+
+?>
